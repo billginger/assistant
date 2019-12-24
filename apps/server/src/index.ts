cc4db27c6a7105c50a6c4c4b8947669bd1ed63d4
Bill <jfg@163.com>
2019-12-24 21:36:51 +0800

    Replace books with users

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,35 +1,28 @@
 import { ApolloServer, gql } from 'apollo-server';
 
-const typeDefs = gql`
-	# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-	# This "Book" type defines the queryable fields for every book in our data source.
-	type Book {
-		title: String
-		author: String
-	}
-
-	# The "Query" type is special: it lists all of the available queries that
-	# clients can execute, along with the return type for each. In this
-	# case, the "books" query returns an array of zero or more Books (defined above).
-	type Query {
-		books: [Book]
-	}
-`;
-
-const books = [
+const users = [
 	{
-		title: 'Harry Potter and the Chamber of Secrets',
-		author: 'J.K. Rowling'
+		name: 'Bill',
+		password: '123456'
 	}, {
-		title: 'Jurassic Park',
-		author: 'Michael Crichton'
+		name: 'Ruby',
+		password: '654321'
 	}
 ];
 
+const typeDefs = gql`
+	type User {
+		name: String
+		password: String
+	}
+	type Query {
+		users: [User]
+	}
+`;
+
 const resolvers = {
 	Query: {
-		books: () => books
+		users: () => users
 	}
 };
 
